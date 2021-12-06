f793b13f924f47ae5074317c51b811083b342c73
Schmitz, Arne <arne.schmitz@cerence.com>
2021-12-06 17:40:46 +0100

    Day 2

--- src/aoc2021/core.clj
+++ src/aoc2021/core.clj
@@ -9,7 +9,13 @@
 (defn read-input
   [input-file]
   (let [input (slurp input-file)]
-    (apply vector (map #(read-string %) (clojure.string/split-lines input))))
+    (apply vector (clojure.string/split-lines input)))
+  )
+
+(defn read-integer-input
+  [input-file]
+  (let [input (slurp input-file)]
+    (apply vector (map #(Integer. %) (clojure.string/split-lines input))))
   )
 
 (defn read-input-csv
@@ -39,11 +45,56 @@
    )
   )
 
+;; day 2
+(defn calc-depth
+  [input]
+  (->>
+   input
+   (map #(clojure.string/split % #" "))
+   (reduce (fn [[x d] [a vs]]
+             (let [v (Integer. vs)]
+               (case a
+                 "forward" [(+ x v) d]
+                 "down" [x (+ d v)]
+                 "up" [x (- d v)]
+                 )
+               )
+             )
+           [0 0]
+           )
+   (apply (fn [x d] (* x d)))
+   )
+  )
+
+(defn calc-aim
+  [input]
+  (->>
+   input
+   (map #(clojure.string/split % #" "))
+   (reduce (fn [[aim x d] [a vs]]
+             (let [v (Integer. vs)]
+               (case a
+                 "forward" [aim (+ x v) (+ d (* aim v))]
+                 "down" [(+ aim v) x d]
+                 "up" [(- aim v) x d]
+                 )
+               )
+             )
+           [0 0 0]
+           )
+   (apply (fn [aim x d] (* x d)))
+   )
+  )
+
 (defn -main
   "Advent of Code 2021."
   [& args]
-  (let [input (read-input "resources/input_1.txt")]
+  (let [input (read-integer-input "resources/input_1.txt")]
     (println "1.1 number of increases = " (calc-increases input))
     (println "1.2 number of increases = " (calc-increases-sliding input))
+    )
+  (let [input (read-input "resources/input_2.txt")]
+    (println "2.1 horizontal position * depth = " (calc-depth input))
+    (println "2.2 horizontal position * depth = " (calc-aim input))
     )
   )
